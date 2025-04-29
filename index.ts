656be96aabd7d23fc6226b5a4fc738859c36ff51
Leandro Morais <lerockit.dev@gmail.com>
2025-04-29 19:19:26 -0300

    fix: remove unused files

--- index.ts
+++
@@ -1,7 +0,0 @@
-import { CommandLineInterfaceAdapter } from './src/adapters/CommandLineInterfaceAdapter'
-import { CLIRoutes } from './src/driver/cli'
-
-const cliAdapter = new CommandLineInterfaceAdapter()
-const routes = new CLIRoutes(cliAdapter)
-
-routes.chooseWord()
